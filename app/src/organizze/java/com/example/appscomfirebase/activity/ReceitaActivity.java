30a5cf6666f821993459bf5f7d326b9a9f1156a0
Agda Loureiro <loureiroagda@gmail.com>
2024-06-18 16:53:03 -0300

    mudnaça da chamada da activity para o metodo binding

--- app/src/organizze/java/com/example/appscomfirebase/activity/ReceitaActivity.java
+++ app/src/organizze/java/com/example/appscomfirebase/activity/ReceitaActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.appscomfirebase.R;
+import com.example.appscomfirebase.databinding.ActivityDespesasBinding;
+import com.example.appscomfirebase.databinding.ActivityReceitaBinding;
 
 public class ReceitaActivity extends AppCompatActivity {
 
+    private ActivityReceitaBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_receita);
+        binding = ActivityReceitaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
