f6162017d23ceda9aad577f5cfc5f25e60609da9
Agda Loureiro <loureiroagda@gmail.com>
2024-06-19 11:57:05 -0300

    Auste na toolbar

--- app/src/organizze/java/com/example/appscomfirebase/activity/PrincipalActivity.java
+++ app/src/organizze/java/com/example/appscomfirebase/activity/PrincipalActivity.java
@@ -1,23 +1,35 @@
 package com.example.appscomfirebase.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.example.appscomfirebase.R;
 import com.example.appscomfirebase.config.ConfiguracaoFirebase;
+import com.example.appscomfirebase.databinding.ActivityMainBinding;
+import com.example.appscomfirebase.databinding.ActivityPrincipalBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private ActivityPrincipalBinding binding;
+    private TextView saldo, pessoa;
 
     private FirebaseAuth autentica = ConfiguracaoFirebase.getAutentificacao();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
+        binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        saldo = findViewById(R.id.p_valor);
+        pessoa = findViewById(R.id.p_titulo);
     }
 
     public void addDespesas(View v){
@@ -34,4 +46,5 @@ public class PrincipalActivity extends AppCompatActivity {
         autentica.signOut();
         startActivities(new Intent[]{new Intent(this, MainActivity.class)});
     }
+
 }
