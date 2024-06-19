4215c094d4bbcc061cac9196f260195f99d98fc6
Agda Loureiro <loureiroagda@gmail.com>
2024-06-19 09:22:33 -0300

    Add do código para salvar a receita no db

--- app/src/organizze/java/com/example/appscomfirebase/activity/ReceitaActivity.java
+++ app/src/organizze/java/com/example/appscomfirebase/activity/ReceitaActivity.java
@@ -1,37 +1,61 @@
 package com.example.appscomfirebase.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.appscomfirebase.R;
-import com.example.appscomfirebase.databinding.ActivityDespesasBinding;
+
+import com.example.appscomfirebase.config.ConfiguracaoFirebase;
+
 import com.example.appscomfirebase.databinding.ActivityReceitaBinding;
+import com.example.appscomfirebase.helper.Base64Custom;
+import com.example.appscomfirebase.helper.DataCustom;
 import com.example.appscomfirebase.model.Movimentacao;
+import com.example.appscomfirebase.model.Usuario;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class ReceitaActivity extends AppCompatActivity {
     private ActivityReceitaBinding binding;
     private Movimentacao movimentacao;
+    private DatabaseReference reference = ConfiguracaoFirebase.getFirebaseDataBase();
+    private FirebaseAuth auth = ConfiguracaoFirebase.getAutentificacao();
+    private double receitaTotal,receita,receitaAtual;
+
+    private String idUsuario = Base64Custom.codificarBase64(auth.getCurrentUser().getEmail());
+    private DatabaseReference usuarioRef = reference.child("usuarios").child(idUsuario);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityReceitaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.txtData.setText(DataCustom.dataAtual());
+        recuperaReceitatotal();
     }
 
-    public void salvarDespesa(View v){
+    public void salvarReceita(View v){
 
         if(validarDados()){
+
+            receita = Double.parseDouble(binding.editValor.getText().toString());
             //Pegando os dados da View
             movimentacao = new Movimentacao();
-            movimentacao.setValor(Double.parseDouble(binding.editValor.getText().toString()));
+            movimentacao.setValor(receita);
             movimentacao.setCategoria(binding.txtCategoria.getText().toString());
             movimentacao.setData(binding.txtData.getText().toString());
             movimentacao.setDescricao(binding.txtDescricao.getText().toString());
             movimentacao.setTipo("d");
 
+            receitaAtual = receita + receitaTotal;
+
+            atualizaReceita();
             //Salvando dados em um nó especifico
             String data = binding.txtData.getText().toString();
             movimentacao.salvar(data);
@@ -64,5 +88,29 @@ public class ReceitaActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Valor não informado!", Toast.LENGTH_LONG).show();
             return false;
         }
+    }
+
+    public void recuperaReceitatotal(){
+
+        //Recuperando a despesa total do bando de dados
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Usuario usuario = snapshot.getValue(Usuario.class);
+                receitaTotal = usuario.getReceitaTotal();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+    public void atualizaReceita(){
+
+        usuarioRef.child("despesaTotal").setValue(receitaAtual);
     }
 }
